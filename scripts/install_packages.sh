ae90da30e5b5dfc71620677ea4dccb8a1e5e05b8
Andrew Hall <ahall@redquasar.net>
2024-08-14 11:11:33 +1000

    Add X11 and Pipewire packages

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -14,6 +14,32 @@ RELEASE="$(rpm -E %fedora)"
 # Development Tools
 #rpm-ostree install cmake cmake-gui autoconf automake binutils bison flex gcc gcc-c++ gdb glibc-devel libtool make pkgconf strace byacc ccache cscope ctags elfutils indent ltrace perf valgrind
 
+# Xorg (no soyland here thanks)
+rpm-ostree install \
+    xorg-x11-server-Xorg \
+    xorg-x11-xinit \
+    xorg-x11-drv-evdev \
+    xorg-x11-drv-libinput \
+    xorg-x11-drv-vesa \
+    xorg-x11-drv-fbdev \
+    xorg-x11-drv-intel \
+    xorg-x11-drv-vmware \
+    xorg-x11-drv-qxl \
+    xorg-x11-xauth \
+    mesa-dri-drivers \
+    mesa-vulkan-drivers \
+    mesa-libGL \
+    mesa-libEGL \
+    mesa-libGLU \
+    glx-utils \
+    xrandr \
+    xsetroot \
+    xclip \
+    xterm
+
+# Multimedia
+rpm-ostree install pipewire-pulseaudio pipewire-utils
+
 # Editing Tools
 rpm-ostree install neovim
 
