edca6061754b160ff60bda62cac1721794f72488
Andrew Hall <ahall@redquasar.net>
2025-10-05 14:55:57 +1000

    Add initial packages, wayland, hyprland, and some other things.

--- Containerfile
+++ Containerfile
@@ -18,6 +18,8 @@ FROM ghcr.io/ublue-os/base-main:latest
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
+RUN /ctx/banner.sh
+
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,13 @@
 
 set -ouex pipefail
 
+# Colours
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+CYAN="\033[0;36m"
+RED="\033[1;31m"
+NC="\033[0m" # No Colour
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +17,41 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+
+# Install critical system libraries and tools
+#echo -e "${CYAN}Installing system libraries and tools...${NC}"
+
+# Add repositories for additional packages
+echo -e "${CYAN}Adding additional package repositories...${NC}"
+dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+# Install NetworkManager and related packages
+echo -e "${CYAN}Installing network management packages...${NC}"
+dnf5 -y install \
+    NetworkManager NetworkManager-wifi \
+    NetworkManager-tui NetworkManager-bluetooth \
+    bluez bluez-tools \
+    rfkill \
+    iw wpa_supplicant \
+    tailscale
+    
+# Install Hyprland and related packages
+echo -e "${CYAN}Installing display manager and desktop environment packages...${NC}"
+dnf5 -y install \
+    hyprland \
+    hypridle hyprlock \
+    xdg-desktop-portal-hyprland \
+    waybar wofi wlogout \
+    wl-clipboard cliphist \
+    grim slurp swappy \
+    brightnessctl playerctl \
+    pavucontrol alsa-utils \
+    polkit-gnome network-manager-applet \
+    qt5-qtwayland qt6-qtwayland \
+    sddm sddm-wayland-generic xorg-xwayland \
+    gnome-keyring
+      
+#dnf5 install -y tmux 
 
 # Use a COPR Example:
 #
@@ -21,4 +62,8 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
+echo -e "${CYAN}Enabling system services...${NC}"
+systemctl enable NetworkManager
 systemctl enable podman.socket
+systemctl enable sddm
+systemctl enable tailscaled
